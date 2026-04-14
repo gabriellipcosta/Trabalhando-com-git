5211e7f6c163b89a65c512271e5c9d8560ec0d41
gabriellipcosta <gabi.pereiracosta@gmail.com>
2026-04-14 13:00:07 -0300

    feat : exercicio finalizado

---
+++ ex03.js
@@ -0,0 +1,9 @@
+const readline = require(`readline-sync`)
+
+let nickname = readline.question(`informe seu nickname:`)
+let jogoFavorito = readline.question(`informe seu jogo favorito:`)
+let pontuacaoAtal = readline.question(`infrome pontuação atual`)
+
+console.log ( `PERFIL GAMER`)
+console.log (`nickname ${nickname}`)
+console.log (``)
